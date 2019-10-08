008d2a69b300d95b2238b09d2ee97c6917edd7d3
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-08 17:51:55 +0700

    'middleware'

--- Updating Data/Update Data/routes/bookRouter.js
+++ Updating Data/Update Data/routes/bookRouter.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 
 function routes(Book) {
   const bookRouter = express.Router();
-  bookRouter.route('/books')
+  bookRouter
+    .route("/books")
     .post((req, res) => {
       const book = new Book(req.body);
 
@@ -21,28 +22,37 @@ function routes(Book) {
         return res.json(books);
       });
     });
-  bookRouter.route('/books/:bookId')
-    .get((req, res) => {
+    
+  bookRouter.use('./books/:bookId', (req, res, next) => {
+    Books.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(401);
+    })
+  })
+
+
+  bookRouter
+    .route("/books/:bookId")
+    .get((req, res) => res.json(req.book))
+    .put((req, res) => {
       Book.findById(req.params.bookId, (err, book) => {
         if (err) {
           return res.send(err);
         }
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.genre = req.body.genre;
+        book.read = req.body.read;
+        book.save();
         return res.json(book);
       });
-    })
-  .put((req, res) => {
-    Book.findById(req.params.bookId, (err, book) => {
-      if(err) {
-        return res.send(err);
-      }
-      book.title = req.body.title;
-      book.author = req.body.author;
-      book.genre = req.body.genre;
-      book.read = req.body.read;
-      book.save();
-      return res.json(book)
-    })
-  })
+    });
 
   return bookRouter;
 }
