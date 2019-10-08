b65d942ed908386916e1a1c9b27b2aadfc8ff920
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-08 16:17:37 +0700

    'cleancode'

--- Postting Data/POST Data/app.js
+++ Postting Data/POST Data/app.js
@@ -2,34 +2,14 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const app = express();
-// const db = mongoose.connect('mongodb://localhost:27017/testing', {useNewUrlParser: true});
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
-bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
+const bookRouter = require('./routes/bookRouter');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-bookRouter.route("/books")
-.post((req, res) => {
-  const book = new Book(req.body);
-
-  book.save();
-  return res.status(201).json(book);
-})
-.get((req, res) => {
-  const query = {};
-  if(req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
-    if (err) {
-      return res.send(err);
-    }
-    return res.json(books);
-  });
-});
 app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
