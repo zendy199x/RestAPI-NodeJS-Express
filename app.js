f07b612234c91df7a04eb6669174d228d3548aec
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-08 14:47:19 +0700

    'search'

--- app.js
+++ app.js
@@ -8,16 +8,12 @@ bookRouter = express.Router();
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
 
-bookRouter.route("/books").get((req, res) => {
-  const query = {};
-  if(req.query.genre) {
-    query.genre = req.query.genre;
-  }
-  Book.find(query, (err, books) => {
+bookRouter.route("/books/:bookId").get((req, res) => {
+  Book.findById(req.params.bookId, (err, book) => {
     if (err) {
       return res.send(err);
     }
-    return res.json(books);
+    return res.json(book);
   });
 });
 app.use("/api", bookRouter);
