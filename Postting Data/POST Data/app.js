c08dc40648012c76ceb4d82233b91ae5d735deb1
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-08 16:27:04 +0700

    'injecting'

--- Postting Data/POST Data/app.js
+++ Postting Data/POST Data/app.js
@@ -5,7 +5,7 @@ const app = express();
 const db = mongoose.connect('mongodb://localhost/bookAPI', { useNewUrlParser: true });
 const port = process.env.PORT || 3000;
 const Book = require("./models/bookModel");
-const bookRouter = require('./routes/bookRouter');
+const bookRouter = require('./routes/bookRouter')(Book);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
