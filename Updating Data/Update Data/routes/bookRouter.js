c941c5de0aad7756d06b5b3a92ed4b34d6ae6f41
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-08 18:50:07 +0700

    'implementingDEL'

--- Updating Data/Update Data/routes/bookRouter.js
+++ Updating Data/Update Data/routes/bookRouter.js
@@ -41,18 +41,31 @@ function routes(Book) {
     .route("/books/:bookId")
     .get((req, res) => res.json(req.book))
     .put((req, res) => {
-      Book.findById(req.params.bookId, (err, book) => {
-        if (err) {
-          return res.send(err);
-        }
+        const { book } = req;
         book.title = req.body.title;
         book.author = req.body.author;
         book.genre = req.body.genre;
         book.read = req.body.read;
         book.save();
         return res.json(book);
-      });
-    });
+      })
+      .patch((req, res) => {
+        const {book} = req;
+        if (req.body._id) {
+          delete req.book._id;
+        }
+        Object.entries(req.body).forEach(item => {
+          const key = item[0];
+          const value = item[1];
+          book[key] = value;
+        });
+        req.book.save((req, res) => {
+          if (err) {
+            return res.send(err);
+          }
+          return res.json(book);
+        });
+      })
 
   return bookRouter;
 }
