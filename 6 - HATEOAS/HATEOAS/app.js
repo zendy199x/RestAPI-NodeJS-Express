aa75dd67cba224f2e9acefc1858fc19edfd95ee5
Nguyễn Văn Đức <nguyenduc.051194@gmail.com>
2019-10-14 14:18:41 +0700

    'HATEOAS'

---
+++ 6 - HATEOAS/HATEOAS/app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+if (process.env.ENV === 'Test') {
+  console.log('This is a test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  console.log('This is for real');
+  const db = mongoose.connect('mongodb://localhost/bookAPI');
+}
+
+
+const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/api', bookRouter);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my Nodemon API!');
+});
+
+app.server = app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
+
+module.exports = app;
---
+++ 6 - HATEOAS/HATEOAS/routes/bookRouter.js
@@ -0,0 +1,75 @@
+/* eslint-disable no-param-reassign */
+const express = require('express');
+const booksController = require('../controllers/booksController');
+
+function routes(Book) {
+  const bookRouter = express.Router();
+  const controller = booksController(Book);
+  bookRouter.route('/books')
+    .post(controller.post)
+    .get(controller.get);
+  bookRouter.use('/books/:bookId', (req, res, next) => {
+    Book.findById(req.params.bookId, (err, book) => {
+      if (err) {
+        return res.send(err);
+      }
+      if (book) {
+        req.book = book;
+        return next();
+      }
+      return res.sendStatus(404);
+    });
+  });
+  bookRouter.route('/books/:bookId')
+    .get((req, res) => {
+      const returnBook = req.book.toJSON();
+
+      returnBook.links = {};
+      const genre = req.book.genre.replace(' ', '%20');
+      returnBook.links.FilterByThisGenre = `http://${req.headers.host}/api/books/?genre=${genre}`;
+      res.json(returnBook);
+    })
+    .put((req, res) => {
+      const { book } = req;
+      book.title = req.body.title;
+      book.author = req.body.author;
+      book.genre = req.body.genre;
+      book.read = req.body.read;
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .patch((req, res) => {
+      const { book } = req;
+      // eslint-disable-next-line no-underscore-dangle
+      if (req.body._id) {
+        // eslint-disable-next-line no-underscore-dangle
+        delete req.body._id;
+      }
+      Object.entries(req.body).forEach((item) => {
+        const key = item[0];
+        const value = item[1];
+        book[key] = value;
+      });
+      req.book.save((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.json(book);
+      });
+    })
+    .delete((req, res) => {
+      req.book.remove((err) => {
+        if (err) {
+          return res.send(err);
+        }
+        return res.sendStatus(204);
+      });
+    });
+  return bookRouter;
+}
+
+module.exports = routes;
